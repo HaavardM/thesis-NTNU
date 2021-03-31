f4ad6ce3265638ad33ed327655251d3f40d88e03
Håvard Skåra Mellbye <havardmellbye@gmail.com>
2021-03-31 14:05:22 +0200

    Add theory

--- master-thesis/figures/introduction-gp/generate.py
+++ master-thesis/figures/introduction-gp/generate.py
@@ -7,38 +7,38 @@ plt.style.use('ggplot')
 x = np.linspace(0, 10)
 y = np.sin(x)
 
-gp = GaussianProcessRegressor(alpha=0.1)
+gp = GaussianProcessRegressor()
 
-
+conf95 = 1.96
 
 f, std = gp.predict(x.reshape(-1, 1), return_std=True)
 mean = f
-ci_high = mean + 2*std
-ci_low = mean - 2*std
+ci_high = mean + conf95*std
+ci_low = mean - conf95*std
 plt.figure()
 plt.plot(x, mean, color="red")
 plt.fill_between(x, ci_high, ci_low, alpha=0.3)
 plt.xlim((min(x), max(x)))
-plt.ylim((-10, 10))
+plt.ylim((-5, 5))
 plt.title("Prior f(x)")
 plt.xlabel("x")
 plt.ylabel("f(x)")
 plt.savefig("prior.png")
 
 
-x_obs = np.array([2.0])
-y_obs = np.array([2.0])
+x_obs = np.array([2.0, 8.0])
+y_obs = np.array([2.0, -2.0])
 gp.fit(x_obs[..., np.newaxis], y_obs)
 f, std = gp.predict(x.reshape(-1, 1), return_std=True)
 mean = f
-ci_high = mean + 2*std
-ci_low = mean - 2*std
+ci_high = mean + conf95*std
+ci_low = mean - conf95*std
 plt.figure()
 plt.plot(x, mean, color="red")
 plt.fill_between(x, ci_high, ci_low, alpha=0.3)
 plt.scatter(x_obs, y_obs, marker="o", color="black")
 plt.xlim((min(x), max(x)))
-plt.ylim((-10, 10))
+plt.ylim((-5, 5))
 plt.title("Posterior f(x)")
 plt.xlabel("x")
 plt.ylabel("f(x)")
